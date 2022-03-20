0a8f592cc2c258d2326b0ce0675bda371b9f7691
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-03-19 17:34:23 -0700

    Adding move to recycle bin error logging

--- recycle_bin.go
+++ recycle_bin.go
@@ -1,21 +1,25 @@
 package vangogh_local_data
 
 import (
+	"github.com/boggydigital/nod"
 	"os"
 	"path/filepath"
 )
 
 func MoveToRecycleBin(absPath string) error {
 	if absPath == "" {
+		nod.Log("move to recycle bin: empty filename")
 		return nil
 	}
 
 	if _, err := os.Stat(absPath); os.IsNotExist(err) {
+		nod.Log("move to recycle bin: file not found: %s", absPath)
 		return nil
 	}
 
 	relPath, err := filepath.Rel(Pwd(), absPath)
 	if err != nil {
+		nod.Log("move to recycle bin: relPath error: base=%s target=%s", Pwd(), absPath)
 		return err
 	}
 

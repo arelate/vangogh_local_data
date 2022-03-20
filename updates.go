6ef3b8dae8bf9aada103c2859dae1cddcebefedf
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-03-19 20:09:57 -0700

    Moving over Updates from _api

---
+++ updates.go
@@ -0,0 +1,67 @@
+package vangogh_local_data
+
+import (
+	"fmt"
+	"github.com/arelate/gog_integration"
+)
+
+var filterNewProductTypes = map[ProductType]bool{
+	Orders: true,
+	//not all licence-products have associated api-products-v1/api-products-v2,
+	//so in some cases we won't get a meaningful information like a title
+	LicenceProducts: true,
+	//both ApiProductsVx are not interesting since they correspond to store-products or account-products
+	ApiProductsV1: true,
+	ApiProductsV2: true,
+}
+
+var filterUpdatedProductTypes = map[ProductType]bool{
+	//most of the Updates are price changes for a sale, not that interesting for recurring sync
+	StoreProducts: true,
+	// wishlist-products are basically store-products, so see above
+	WishlistProducts: true,
+	//meaningful Updates for account products come from details, not account-products
+	AccountProducts: true,
+	//same as above for those product types
+	ApiProductsV1: true,
+	ApiProductsV2: true,
+}
+
+func Updates(mt gog_integration.Media, since int64) (map[string]map[string]bool, error) {
+	updates := make(map[string]map[string]bool, 0)
+
+	for _, pt := range LocalProducts() {
+
+		if filterNewProductTypes[pt] {
+			continue
+		}
+
+		vr, err := NewReader(pt, mt)
+		if err != nil {
+			return updates, err
+		}
+
+		categorize(vr.CreatedAfter(since),
+			fmt.Sprintf("new in %s", pt.HumanReadableString()),
+			updates)
+
+		if filterUpdatedProductTypes[pt] {
+			continue
+		}
+
+		categorize(vr.ModifiedAfter(since, true),
+			fmt.Sprintf("updated in %s", pt.HumanReadableString()),
+			updates)
+	}
+
+	return updates, nil
+}
+
+func categorize(ids []string, cat string, updates map[string]map[string]bool) {
+	for _, id := range ids {
+		if updates[cat] == nil {
+			updates[cat] = make(map[string]bool, 0)
+		}
+		updates[cat][id] = true
+	}
+}

62e57020e9266e8ebb2b747da4373880faea8aaf
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-04-09 23:34:06 -0700

    Moving func to extract description items from gog_integration

---
+++ extract_desc_items.go
@@ -0,0 +1,9 @@
+package vangogh_local_data
+
+import "regexp"
+
+var re = regexp.MustCompile(`https://items.gog.com/([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)`)
+
+func ExtractDescItems(desc string) []string {
+	return re.FindAllString(desc, -1)
+}

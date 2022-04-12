a46ddfc75a3b3f725fcae4b2fda623aa24d665af
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-04-12 00:32:38 -0700

    Fixing URLs containing spaces and Unicode characters

--- extract_desc_items.go
+++ extract_desc_items.go
@@ -2,7 +2,7 @@ package vangogh_local_data
 
 import "regexp"
 
-var re = regexp.MustCompile(`https://items.gog.com/([-a-zA-Z0-9()!@:%_,'\+.~#?&\/\/=]*)`)
+var re = regexp.MustCompile(`https://items.gog.com/([-\pL0-9()!@:%_,'™\ \+.~#?&\/\/=]*)`)
 
 func ExtractDescItems(desc string) []string {
 	return re.FindAllString(desc, -1)

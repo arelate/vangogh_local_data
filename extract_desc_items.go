550c79ff42400891d80c6b128f29cc8b0b5ac358
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-04-12 12:35:42 -0700

    Attempting to simplify regex and to capture currently unknown edge cases

--- extract_desc_items.go
+++ extract_desc_items.go
@@ -2,7 +2,7 @@ package vangogh_local_data
 
 import "regexp"
 
-var re = regexp.MustCompile(`https://items.gog.com/([-\pL0-9()!@:%_,'™\ \+.~#?&\/\/=]*)`)
+var re = regexp.MustCompile(`https://items.gog.com/([^"']*?).*?(?="|$)`)
 
 func ExtractDescItems(desc string) []string {
 	return re.FindAllString(desc, -1)

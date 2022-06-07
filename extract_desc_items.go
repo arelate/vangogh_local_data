4c5c212ec71db7a4cac6a675f8bc7508ee0848fd
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-06-06 21:51:53 -0700

    Fixing certain GOG.com game links using /en/game/slug format (e.g. Stellaris Expansions Pack)

--- extract_desc_items.go
+++ extract_desc_items.go
@@ -4,7 +4,7 @@ import "regexp"
 
 var (
 	descItems     = regexp.MustCompile(`https://items.gog.com/([-\pL0-9()!@:%_,'™\ \+\[\]\x{2013}/u.~#?&\/\/=]*)`)
-	descGameLinks = regexp.MustCompile(`https://www.gog.com/game/([-\pL0-9()!@:%_,'™\ \+\[\]\x{2013}/u.~#?&\/\/=]*)`)
+	descGameLinks = regexp.MustCompile(`https://www.gog.com/(en/)?game/([-\pL0-9()!@:%_,'™\ \+\[\]\x{2013}/u.~#?&\/\/=]*)`)
 )
 
 func ExtractDescItems(desc string) []string {

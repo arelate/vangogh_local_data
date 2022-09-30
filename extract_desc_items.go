dac6825ceccf3e1c061402a91443a42817ae0445
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-09-30 11:33:54 -0700

    Fixing CodeQL error

--- extract_desc_items.go
+++ extract_desc_items.go
@@ -3,8 +3,8 @@ package vangogh_local_data
 import "regexp"
 
 var (
-	descItems     = regexp.MustCompile(`https://items\.gog.com/([-\pL0-9()!@:%_,'™\ \+\[\]\x{2013}/u.~#?&\/\/=]*)`)
-	descGameLinks = regexp.MustCompile(`https://www\.gog.com/(en/)?game/([-\pL0-9()!@:%_,'™\ \+\[\]\x{2013}/u.~#?&\/\/=]*)`)
+	descItems     = regexp.MustCompile(`https://items\.gog\.com/([-\pL0-9()!@:%_,'™\ \+\[\]\x{2013}/u.~#?&\/\/=]*)`)
+	descGameLinks = regexp.MustCompile(`https://www\.gog\.com/(en/)?game/([-\pL0-9()!@:%_,'™\ \+\[\]\x{2013}/u.~#?&\/\/=]*)`)
 )
 
 func ExtractDescItems(desc string) []string {
